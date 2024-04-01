da15a14c8a46e43e68fa38412f88bf357d841814
Ben Ferenchak <ben.ferenchak@gmail.com>
2024-04-01 19:54:58 -0400

    Fixing bug in calculate_speed_reward

--- src/reward_modifiers.py
+++ src/reward_modifiers.py
@@ -5,6 +5,9 @@ def calculate_speed_reward(params, initial_reward, waypoints, target_speed, rewa
     if params['closest_waypoints'][0] not in waypoints:
         return initial_reward
 
+    if max_reward_multiplier < 1:
+        raise InvalidInput('Max reward multiplier must be greater than 1')
+
     speed = params['speed']
     speed_diff = abs(speed - target_speed)
 
@@ -12,7 +15,11 @@ def calculate_speed_reward(params, initial_reward, waypoints, target_speed, rewa
         return initial_reward
 
     percent_of_max_multiplier = (rewardable_speed_range - speed_diff) / rewardable_speed_range
-    new_reward = initial_reward * max_reward_multiplier * percent_of_max_multiplier
+    new_reward = initial_reward * (1 + (max_reward_multiplier - 1) * percent_of_max_multiplier)
     print(f'Reward: {initial_reward} -> {new_reward}, Speed: {speed}, Target Speed: {target_speed}, Rewardable Speed Range: {rewardable_speed_range}, Max Reward Multiplier: {max_reward_multiplier}')
 
     return new_reward
+
+
+class InvalidInput(Exception):
+    pass
--- test/test_reward_modifiers.py
+++ test/test_reward_modifiers.py
@@ -1,11 +1,11 @@
 import unittest
 
-from reward_modifiers import calculate_speed_reward
+from reward_modifiers import calculate_speed_reward, InvalidInput
 
 
 class TestRewardModifiers(unittest.TestCase):
 
-    def test_speed_reward_calculation(self):
+    def test_calculate_speed_reward(self):
 
         # If outside targeted waypoints just return initial reward
         self.assertEqual(1, calculate_speed_reward(params={'speed': 5, 'closest_waypoints': [0]}, initial_reward=1, waypoints={1}, target_speed=5, rewardable_speed_range=10, max_reward_multiplier=10))
@@ -15,14 +15,22 @@ class TestRewardModifiers(unittest.TestCase):
         self.assertEqual(20, calculate_speed_reward(params={'speed': 5, 'closest_waypoints': [0]}, initial_reward=1, waypoints={0}, target_speed=5, rewardable_speed_range=10, max_reward_multiplier=20))
 
         # If in target waypoints and within speed range, give reward multiple equal to proximity to the target speed
-        self.assertEqual(5, calculate_speed_reward(params={'speed': 5, 'closest_waypoints': [0]}, initial_reward=1, waypoints={0}, target_speed=10, rewardable_speed_range=10, max_reward_multiplier=10))
-        self.assertEqual(6, calculate_speed_reward(params={'speed': 6, 'closest_waypoints': [0]}, initial_reward=1, waypoints={0}, target_speed=10, rewardable_speed_range=10, max_reward_multiplier=10))
-        self.assertEqual(7, calculate_speed_reward(params={'speed': 7, 'closest_waypoints': [0]}, initial_reward=1, waypoints={0}, target_speed=10, rewardable_speed_range=10, max_reward_multiplier=10))
-        self.assertEqual(8, calculate_speed_reward(params={'speed': 8, 'closest_waypoints': [0]}, initial_reward=1, waypoints={0}, target_speed=10, rewardable_speed_range=10, max_reward_multiplier=10))
-        self.assertEqual(9, calculate_speed_reward(params={'speed': 9, 'closest_waypoints': [0]}, initial_reward=1, waypoints={0}, target_speed=10, rewardable_speed_range=10, max_reward_multiplier=10))
+        self.assertAlmostEqual(1, calculate_speed_reward(params={'speed': 3, 'closest_waypoints': [0]}, initial_reward=1, waypoints={0}, target_speed=5, rewardable_speed_range=2, max_reward_multiplier=2))
+        self.assertAlmostEqual(1.05, calculate_speed_reward(params={'speed': 3.1, 'closest_waypoints': [0]}, initial_reward=1, waypoints={0}, target_speed=5, rewardable_speed_range=2, max_reward_multiplier=2))
+        self.assertAlmostEqual(1.5, calculate_speed_reward(params={'speed': 4, 'closest_waypoints': [0]}, initial_reward=1, waypoints={0}, target_speed=5, rewardable_speed_range=2, max_reward_multiplier=2))
+        self.assertAlmostEqual(1.95, calculate_speed_reward(params={'speed': 4.9, 'closest_waypoints': [0]}, initial_reward=1, waypoints={0}, target_speed=5, rewardable_speed_range=2, max_reward_multiplier=2))
+        self.assertAlmostEqual(2, calculate_speed_reward(params={'speed': 5, 'closest_waypoints': [0]}, initial_reward=1, waypoints={0}, target_speed=5, rewardable_speed_range=2, max_reward_multiplier=2))
+        self.assertAlmostEqual(1.95, calculate_speed_reward(params={'speed': 5.1, 'closest_waypoints': [0]}, initial_reward=1, waypoints={0}, target_speed=5, rewardable_speed_range=2, max_reward_multiplier=2))
+        self.assertAlmostEqual(1.5, calculate_speed_reward(params={'speed': 6, 'closest_waypoints': [0]}, initial_reward=1, waypoints={0}, target_speed=5, rewardable_speed_range=2, max_reward_multiplier=2))
+        self.assertAlmostEqual(1.05, calculate_speed_reward(params={'speed': 6.9, 'closest_waypoints': [0]}, initial_reward=1, waypoints={0}, target_speed=5, rewardable_speed_range=2, max_reward_multiplier=2))
+        self.assertAlmostEqual(1, calculate_speed_reward(params={'speed': 7, 'closest_waypoints': [0]}, initial_reward=1, waypoints={0}, target_speed=5, rewardable_speed_range=2, max_reward_multiplier=2))
 
         # If outside the rewardable range the initial reward value should be returned
         self.assertEqual(1, calculate_speed_reward(params={'speed': 5, 'closest_waypoints': [0]}, initial_reward=1, waypoints={0}, target_speed=10, rewardable_speed_range=2, max_reward_multiplier=10))
+
+        # If max reward multiplier is < 1 InvalidInput should be raised
+        with self.assertRaises(InvalidInput):
+            calculate_speed_reward(params={'speed': 5, 'closest_waypoints': [0]}, initial_reward=1, waypoints={0}, target_speed=5, rewardable_speed_range=10, max_reward_multiplier=0.9)
 
 
 if __name__ == '__main__':
